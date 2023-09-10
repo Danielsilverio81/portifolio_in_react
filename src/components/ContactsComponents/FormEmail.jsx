a6aab71f58503aaad26338671e47966b021bba6e
danielSdev <danielsilverio81@gmail.com>
2023-09-10 14:46:07 -0300

    task: add email js for form submit

--- src/components/ContactsComponents/FormEmail.jsx
+++ src/components/ContactsComponents/FormEmail.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import propTypes from "prop-types";
+import emailJs from "@emailjs/browser";
 
 FormEmail.propTypes = {
   activeForm: propTypes.bool,
@@ -12,16 +13,34 @@ export default function FormEmail({ activeForm }) {
   const [emailValue, setEmailValue] = useState("");
   const [messageValue, setMessageValue] = useState("");
 
+  const [submit, setSubmit] = useState(false);
+
+  const templateParams = {
+    from_name: nameValue,
+    message: messageValue,
+    email: emailValue
+  }
+  const sendEmail = (e) => {
+    e.preventDefault()
+    emailJs.send("service_o2rb8f2", "template_3oia459", templateParams, "S7YLoWO4yG_U6yijc")
+    .then(() => {
+      setSubmit(true)
+      setNameValue('')
+      setEmailValue('')
+      setMessageValue('')
+    }, (err) => alert(`Error: ${err}`)
+  )}
+
   
 
   return (
     <div
       className={`
-      w-full h-auto border-2 border-red-300 bg-white p-4 rounded-md 
+      w-full h-fit border-2 border-red-300 bg-white p-4 rounded-md  
       ${activeForm ? "inline" : "hidden"}
       `}
     >
-      <form >
+      <form onSubmit={sendEmail} >
         <label className="font-bold" htmlFor="nameId">
           Nome:
         </label>
@@ -58,12 +77,16 @@ export default function FormEmail({ activeForm }) {
 
         <button
           className="
-        bg-emerald-400 p-2 border border-black rounded-md
+        bg-emerald-400 p-2 border border-black rounded-md w-[90%] mx-auto mt-2 mb-8
         "
           type="submit"
         >
           Enviar
         </button>
+        <span className={`
+        ${submit ? 'inline':'hidden'} text-emerald-500 bg-darkBgColor w-[90%]
+        text-center p-3 mx-24 border border-emerald-300
+        `}>Enviado com sucesso!!</span>
       </form>
     </div>
   );
