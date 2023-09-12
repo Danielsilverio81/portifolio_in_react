e46ccdf2bbabafccb8b7b74941839c71ede0123e
danielSdev <danielsilverio81@gmail.com>
2023-09-12 10:58:52 -0300

    change: browser router

--- src/router.jsx
+++ src/router.jsx
@@ -1,26 +1,19 @@
-import { createBrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RootLayout from "./routers/RootLayout";
 import Home from "./routers/Home";
 import About from "./routers/About";
 import Projects from "./routers/Projects";
 import Contacts from "./routers/Contacts";
 
-export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <RootLayout/>,
-        children: [{
-            index: true,
-            element: <Home />
-        }, {
-            path: "about",
-            element: <About />
-        }, {
-            path: "projects",
-            element: <Projects />
-        }, {
-            path: "contacts",
-            element: <Contacts/>
-        }]
-    }
-])
+export const router = (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="contacts" element={<Contacts />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
