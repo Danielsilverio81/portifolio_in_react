55fe643959beb143b19052ed21baaacd794df092
danielSdev <danielsilverio81@gmail.com>
2023-09-21 20:36:11 -0300

    update: projects

--- src/components/ProjectsComponents/ShowMyProjects.jsx
+++ src/components/ProjectsComponents/ShowMyProjects.jsx
@@ -2,6 +2,18 @@ import HoverElementsImage from "./CreatePreview";
 const data = [
   {
     id: 1,
+    name: "App de Previsão do Tempo usando a Api Open Weather",
+    gif: "/gifs/predicting-the-weather.gif",
+    link: "https://predicting-the-weather-with-the-open-weather-api.vercel.app/",
+    description: `Projeto de Autoria Propriá... Usando a api Open Weather Criei uma pagina
+    que traz as informações atuais do dia, dos próximos dois dias de 3 em 3 horas e alguns dados 
+    interessantes sobre como sera o dia. foi usado React com Vite, css e Axios para Requisições.`,
+    gitHub:
+      "https://github.com/Danielsilverio81/predicting_the_weather_with_the_open_WeatherAPI",
+    imageDefault: "/images/project_preview_openweather.png",
+  },
+  {
+    id: 2,
     name: "Conversor de Moedas Fetch Api",
     gif: "/gifs/Currency_Converter_Fetch_Api.gif",
     link: "https://currency-converter-using-a-fetch-api.vercel.app/",
@@ -11,10 +23,10 @@ const data = [
     uso de plugins e loaders além do babel para deixar compatível com navegadores antigos.`,
     gitHub:
       "https://github.com/Danielsilverio81/currency_converter_using_a_fetch_api",
-    imageDefault: "/images/project_preview_1.png",
+    imageDefault: "/images/project_preview_cm.png",
   },
   {
-    id: 2,
+    id: 3,
     name: "Site de Clinica Medica Fictício",
     gif: "/gifs/medical_clinic_website.gif",
     link: "https://responsive-website-beta.vercel.app/",
@@ -23,10 +35,10 @@ const data = [
       assim aprendendo bastante sobre a base pois para obter este resultado eu mesmo tive que criar 
       a estrutura e estilização, sem ajuda ate o final do projeto, usando figma para inspeção do template.`,
     gitHub: "https://github.com/Danielsilverio81/Responsive_website_project",
-    imageDefault: "/images/project_preview_2.png",
+    imageDefault: "/images/project_preview_emergency.png",
   },
   {
-    id: 3,
+    id: 4,
     name: "Landing Page Workit Desafio FrontEnd Mentor",
     gif: "/gifs/Landing_page_copies_challenge_Front_End_Mentor.gif",
     link: "https://front-end-mentor-landing-page-workit-challenge.vercel.app/",
@@ -35,10 +47,10 @@ const data = [
       todas as telas, com um html semântico.`,
     gitHub:
       "https://github.com/Danielsilverio81/FrontEnd_Mentor_Landing_page-workit_challenge",
-    imageDefault: "/images/project_preview_3.png",
+    imageDefault: "/images/project_preview_workit.png",
   },
   {
-    id: 4,
+    id: 5,
     name: "Landing Page Equalizer Desafio FrontEnd Mentor",
     gif: "/gifs/Landing_page_Equalizer_Challenge_Front_End_Mentor.gif",
     link: "https://equalizerlandingpage.vercel.app/",
@@ -47,10 +59,10 @@ const data = [
      todas as telas, com um html semântico. aprende muito sobre o posicionamento de elementos na tela por este
       projeto.`,
     gitHub: "https://github.com/Danielsilverio81/equalizer-landingpage-project",
-    imageDefault: "/images/project_preview_4.png",
+    imageDefault: "/images/project_preview_equali.png",
   },
   {
-    id: 5,
+    id: 6,
     name: "Flappy Bird Copia em Javascript puro",
     gif: "/gifs/flappy_bird_game_in_js.gif",
     link: "https://flappy-copy-in-js.vercel.app/",
@@ -59,10 +71,10 @@ const data = [
      e pegar as entradas do teclado não apenas do mouse, com isso aprende bastante sobre a manipulação do Dom
      e a criação de elementos dinamicamente dando vida a pagina.`,
     gitHub: "https://github.com/Danielsilverio81/Flappy-B_in_JS",
-    imageDefault: "/images/project_preview_5.png",
+    imageDefault: "/images/project_preview_bird.png",
   },
   {
-    id: 6,
+    id: 7,
     name: "Agencia de Viagens Formulário",
     gif: "/gifs/Travel_agency_form.gif",
     link: "https://travel-agency-form-q60gk6d23-danielsilverio81.vercel.app/",
@@ -70,10 +82,10 @@ const data = [
     usando html, css e javascript onde aprendo sobre o uso de formulário e como fazer a validação usando 
     regex e try catch no javascript`,
     gitHub: "https://github.com/Danielsilverio81/Travel_agency_form",
-    imageDefault: "/images/project_preview_6.png",
+    imageDefault: "/images/project_preview_form.png",
   },
   {
-    id: 7,
+    id: 8,
     name: "Calculadora de IMC",
     gif: "/gifs/imc_calc.gif",
     link: "https://imc-calculator-in-pure-javascript-c7ks2wfpm-danielsilverio81.vercel.app/",
@@ -81,10 +93,10 @@ const data = [
     a ter uma  forma rapida de calcular seu imc e saber sua situação atual com base no seu IMC.`,
     gitHub:
       "https://github.com/Danielsilverio81/IMC_calculator_in_pure_javascript",
-    imageDefault: "/images/project_preview_7.png",
+    imageDefault: "/images/project_preview_Imc.png",
   },
   {
-    id: 8,
+    id: 9,
     name: "SpaceX copia",
     gif: "/gifs/spaceX_WebSite_copy.gif",
     link: "https://my-spacex-website-version.vercel.app/index.html",
@@ -92,7 +104,7 @@ const data = [
     copia do site da SpaceX usando Bootstrap Html Css, Sass e da biblioteca AOS para textos animados.`,
     gitHub:
       "https://github.com/Danielsilverio81/My_spacex_website_version_in_bootstrap_and_sass",
-    imageDefault: "/images/project_preview_8.png",
+    imageDefault: "/images/project_preview_space.png",
   },
 ];
 
