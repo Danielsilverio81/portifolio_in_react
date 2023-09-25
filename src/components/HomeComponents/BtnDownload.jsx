1ac60eed6cdaa82edea053dbbb91e13a15bdebff
danielSdev <danielsilverio81@gmail.com>
2023-09-25 19:10:56 -0300

    fix: bug curriculo

--- src/components/HomeComponents/BtnDownload.jsx
+++ src/components/HomeComponents/BtnDownload.jsx
@@ -3,7 +3,7 @@ import { AiOutlineDownload } from "react-icons/ai";
 export default function BtnDownload() {
   return (
     <div className="w-48 mt-5 mb-5">
-    <a href="/src/pdf/Currículo Básico profissional neutro azul e branco (1).pdf" download>
+    <a href="/pdf/curriculo.pdf" download>
       <button className="
       h-auto p-1 bg-darkBgColor text-zinc-100 rounded-l-lg flex
       items-center justify-center gap-1 border border-black
---
+++ src/components/ProjectsComponents/dataProjects/data.js
@@ -0,0 +1,108 @@
+export const data = [
+    {
+      id: 1,
+      name: "App de Previsão do Tempo usando a Api Open Weather",
+      gif: "/gifs/predicting-the-weather.gif",
+      link: "https://predicting-the-weather-with-the-open-weather-api.vercel.app/",
+      description: `Projeto de Autoria Propriá... Usando a api Open Weather Criei uma pagina
+      que traz as informações atuais do dia, dos próximos dois dias de 3 em 3 horas e alguns dados 
+      interessantes sobre como sera o dia. foi usado React com Vite, css e Axios para Requisições.`,
+      gitHub:
+        "https://github.com/Danielsilverio81/predicting_the_weather_with_the_open_WeatherAPI",
+      imageDefault: "/images/project_preview_openweather.png",
+    },
+    {
+      id: 2,
+      name: "Conversor de Moedas Fetch Api",
+      gif: "/gifs/Currency_Converter_Fetch_Api.gif",
+      link: "https://currency-converter-using-a-fetch-api.vercel.app/",
+      description: `Projeto de Autoria Propriá...
+      Neste app web Consumo uma Api de Cambio com Fetch, usando webpack para empacotar os arquivos Javascript e comprimir o css final
+      pre processado pelo Sass, melhorando a performance do app, também pude aprender como configurar o webpack e 
+      uso de plugins e loaders além do babel para deixar compatível com navegadores antigos.`,
+      gitHub:
+        "https://github.com/Danielsilverio81/currency_converter_using_a_fetch_api",
+      imageDefault: "/images/project_preview_cm.png",
+    },
+    {
+      id: 3,
+      name: "Site de Clinica Medica Fictício",
+      gif: "/gifs/medical_clinic_website.gif",
+      link: "https://responsive-website-beta.vercel.app/",
+      description: `Projeto de estudos feito na plataforma DevMedia, pude aprender o uso das
+       media queries para responsividade e a paginação de uma pagina usando simplesmente html e css
+        assim aprendendo bastante sobre a base pois para obter este resultado eu mesmo tive que criar 
+        a estrutura e estilização, sem ajuda ate o final do projeto, usando figma para inspeção do template.`,
+      gitHub: "https://github.com/Danielsilverio81/Responsive_website_project",
+      imageDefault: "/images/project_preview_emergency.png",
+    },
+    {
+      id: 4,
+      name: "Landing Page Workit Desafio FrontEnd Mentor",
+      gif: "/gifs/Landing_page_copies_challenge_Front_End_Mentor.gif",
+      link: "https://front-end-mentor-landing-page-workit-challenge.vercel.app/",
+      description: `Desafio do FrontEnd Mentor onde usei html css e figma, o desafio consiste em replicar
+       o template o mais proximo possível do original fornecido, fazendo uso do figma e deixa-lo responsivo a
+        todas as telas, com um html semântico.`,
+      gitHub:
+        "https://github.com/Danielsilverio81/FrontEnd_Mentor_Landing_page-workit_challenge",
+      imageDefault: "/images/project_preview_workit.png",
+    },
+    {
+      id: 5,
+      name: "Landing Page Equalizer Desafio FrontEnd Mentor",
+      gif: "/gifs/Landing_page_Equalizer_Challenge_Front_End_Mentor.gif",
+      link: "https://equalizerlandingpage.vercel.app/",
+      description: `Desafio do FrontEnd Mentor onde usei html css e figma, o desafio consiste em replicar
+      o template o mais proximo possível do original fornecido, fazendo uso do figma e deixa-lo responsivo a
+       todas as telas, com um html semântico. aprende muito sobre o posicionamento de elementos na tela por este
+        projeto.`,
+      gitHub: "https://github.com/Danielsilverio81/equalizer-landingpage-project",
+      imageDefault: "/images/project_preview_equali.png",
+    },
+    {
+      id: 6,
+      name: "Flappy Bird Copia em Javascript puro",
+      gif: "/gifs/flappy_bird_game_in_js.gif",
+      link: "https://flappy-copy-in-js.vercel.app/",
+      description: `Projeto feito em Curso de Web Moderno da Udemy, no final do projeto fiz algumas modificações,
+       criando dificuldades e uma tela de inicio, além de reformular algumas funções para atender a mais dispositivos
+       e pegar as entradas do teclado não apenas do mouse, com isso aprende bastante sobre a manipulação do Dom
+       e a criação de elementos dinamicamente dando vida a pagina.`,
+      gitHub: "https://github.com/Danielsilverio81/Flappy-B_in_JS",
+      imageDefault: "/images/project_preview_bird.png",
+    },
+    {
+      id: 7,
+      name: "Agencia de Viagens Formulário",
+      gif: "/gifs/Travel_agency_form.gif",
+      link: "https://travel-agency-form-q60gk6d23-danielsilverio81.vercel.app/",
+      description: `Projeto desenvolvido na Plataforma DevMedia um site fictício para uma agencia de viagens
+      usando html, css e javascript onde aprendo sobre o uso de formulário e como fazer a validação usando 
+      regex e try catch no javascript`,
+      gitHub: "https://github.com/Danielsilverio81/Travel_agency_form",
+      imageDefault: "/images/project_preview_form.png",
+    },
+    {
+      id: 8,
+      name: "Calculadora de IMC",
+      gif: "/gifs/imc_calc.gif",
+      link: "https://imc-calculator-in-pure-javascript-c7ks2wfpm-danielsilverio81.vercel.app/",
+      description: `Um site de código e design Próprio com o intuito de ajudar algumas pessoas próximas,
+      a ter uma  forma rapida de calcular seu imc e saber sua situação atual com base no seu IMC.`,
+      gitHub:
+        "https://github.com/Danielsilverio81/IMC_calculator_in_pure_javascript",
+      imageDefault: "/images/project_preview_Imc.png",
+    },
+    {
+      id: 9,
+      name: "SpaceX copia",
+      gif: "/gifs/spaceX_WebSite_copy.gif",
+      link: "https://my-spacex-website-version.vercel.app/index.html",
+      description: `Projeto desenvolvido na One Bit Code Plataforma de ensino FullStack onde estudo atualmente,
+      copia do site da SpaceX usando Bootstrap Html Css, Sass e da biblioteca AOS para textos animados.`,
+      gitHub:
+        "https://github.com/Danielsilverio81/My_spacex_website_version_in_bootstrap_and_sass",
+      imageDefault: "/images/project_preview_space.png",
+    },
+  ];
